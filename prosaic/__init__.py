9581b0d5be465ff921e4e6a57d7744c435b18766
nathaniel smith <nathanielksmith@gmail.com>
2015-09-23 11:52:10 -0700

    clean up command processing a bit

--- prosaic/__init__.py
+++ prosaic/__init__.py
@@ -14,24 +14,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
-from argparse import ArgumentParser, FileType
 from os.path import join, exists, dirname
 
 from sh import cp, mkdir
 
 import prosaic.commands as cmd
-from prosaic.cfg import PROSAIC_HOME, TEMPLATES, MONGO_HOST, MONGO_PORT, DEFAULT_DB
-
-# TODO for love of god put this in readme, not here:
-## CLI UI
-
-## prosaic poem new -h <host> -p <port> -d <db> -t <tmpl> -o <name>
-## prosaic corpus ls -hpd
-## prosaic corpus loadfile -h <host> -p <port> -d <db> <filename>
-## prosaic corpus rm -h <host> -p <port> -d <db>
-## prosaic template new <name>
-## prosaic template ls
-## prosaic template rm <name>
+from prosaic.cfg import PROSAIC_HOME, DEFAULT_DB
 
 def is_installed():
     return all(map(exists, [PROSAIC_HOME,
@@ -42,90 +30,10 @@ def install():
     mkdir(PROSAIC_HOME)
     cp('-r', template_source, PROSAIC_HOME)
 
-class ProsaicArgParser(ArgumentParser):
-    def add_dbhost(self):
-        self.add_argument('--host', action='store', default=MONGO_HOST)
-        return self
-
-    def add_dbport(self):
-        self.add_argument('-p', '--port', action='store', default=MONGO_PORT)
-        return self
-
-    def add_dbname(self):
-        self.add_argument('-d', '--dbname', action='store', default=DEFAULT_DB)
-        return self
-
-    def add_db(self):
-        self.add_dbhost().add_dbport().add_dbname()
-        return self
-
-    def add_argument(self, *args, **kwargs):
-        '''literally just here for chaining lol'''
-        super().add_argument(*args, **kwargs)
-        return self
-
-    def set_defaults(self, *args, **kwargs):
-        '''literally just here for chaining lol'''
-        super().set_defaults(*args, **kwargs)
-        return self
-
-def init_arg_parser():
-    top_level_parser = ProsaicArgParser(prog='prosaic')
-    subparsers = top_level_parser.add_subparsers()
-
-    corpus_parser = subparsers.add_parser('corpus')
-    corpus_subs = corpus_parser.add_subparsers()
-
-    poem_parser = subparsers.add_parser('poem')
-    poem_subs = poem_parser.add_subparsers()
-
-    template_parser = subparsers.add_parser('template')
-    template_subs = template_parser.add_subparsers()
-
-    top_level_parser.add_argument('infile', nargs='?', type=FileType('r'), default=sys.stdin)
-
-    # corpus commands
-    corpus_subs.add_parser('ls').set_defaults(func=cmd.corpus_ls).add_db()
-    corpus_subs.add_parser('loadfile') \
-               .set_defaults(func=cmd.corpus_loadfile) \
-               .add_argument('path', action='store') \
-               .add_db()
-    corpus_subs.add_parser('rm') \
-               .set_defaults(func=cmd.corpus_rm) \
-               .add_dbhost() \
-               .add_dbport() \
-               .add_argument('dbname', default=DEFAULT_DB, action='store')
-
-    # poem commands
-    poem_subs.add_parser('new') \
-             .set_defaults(func=cmd.poem_new) \
-             .add_argument('-r', '--tmpl_raw', action='store_true') \
-             .add_argument('-o', '--output', action='store') \
-             .add_argument('-t', '--tmpl', action='store', default='haiku') \
-             .add_db()
-
-    # template commands
-    template_subs.add_parser('ls').set_defaults(func=cmd.template_ls)
-    template_subs.add_parser('rm') \
-                 .set_defaults(func=cmd.template_rm) \
-                 .add_argument('tmplname', action='store')
-    template_subs.add_parser('new') \
-                 .set_defaults(func=cmd.template_new) \
-                 .add_argument('tmplname', action='store')
-    template_subs.add_parser('edit') \
-                 .set_defaults(func=cmd.template_edit) \
-                 .add_argument('tmplname', action='store')
-
-    return top_level_parser
-
 def main():
     if not is_installed():
         install()
 
-    argument_parser = init_arg_parser()
-    parsed_args = argument_parser.parse_args()
-    # TODO try/except for bad subcommand
-    parsed_args.func(parsed_args)
-    return 0
+    return cmd.initialize_arg_parser().dispatch()
 
 if __name__ == '__main__': sys.exit(main())
--- prosaic/commands.py
+++ prosaic/commands.py
@@ -12,94 +12,195 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from argparse import ArgumentParser, FileType
 from json import loads
 from os import environ, listdir
 from os.path import join
 from subprocess import call
+import sys
 
 from pymongo import MongoClient
 from sh import rm, cp
 
 from prosaic.nyarlathotep import process_text
 from prosaic.cthulhu import poem_from_template
-from prosaic.cfg import DEFAULT_TEMPLATE_EXT, TEMPLATES, EXAMPLE_TEMPLATE
+from prosaic.cfg import DEFAULT_TEMPLATE_EXT, TEMPLATES, EXAMPLE_TEMPLATE, MONGO_HOST, MONGO_PORT, DEFAULT_DB
 from prosaic.util import slurp
 
-# TODO refactor all of this stuff to be in the ProsaicArgParser class.
+class ProsaicArgParser(ArgumentParser):
+    editor = environ.get('EDITOR')
+    _template = None
+    _client = None
 
-def read_template(name):
-    if name.startswith('/'):
-        path = name
-    else:
-        path = join(TEMPLATES, '{}.{}'.format(name, DEFAULT_TEMPLATE_EXT))
-    return slurp(path)
 
-def args_to_template(parsed_args):
-    if parsed_args.tmpl_raw:
-        template = ''.join(parsed_args.infile.readlines())
-    else:
-        template = read_template(parsed_args.tmpl)
+    # Helpers and properties:
 
-    return loads(template)
+    @property
+    def client(self):
+        if not self.args:
+            return None
+        return self._client if self._client else MongoClient(self.args.host, self.args.port)
 
-def args_to_client(parsed_args):
-    return MongoClient(parsed_args.host, parsed_args.port)
+    @property
+    def db(self):
+        if not self.args:
+            return None
+        return self.client[self.args.dbname].phrases
 
-def args_to_db(parsed_args):
-    client = args_to_client(parsed_args)
-    return client[parsed_args.dbname].phrases
+    @property
+    def template(self):
+        if not self.args:
+            return None
 
-def corpus_ls(parsed_args):
-    client = args_to_client(parsed_args)
-    for dbname in client.database_names():
-        print(dbname)
+        if self._template:
+            return self._template
 
-def corpus_rm(parsed_args):
-    client = args_to_client(parsed_args)
-    client.drop_database(parsed_args.dbname)
+        if self.args.tmpl_raw:
+            template_path = ''.join(self.args.infile.readlines())
+        else:
+            template_path = self.read_template(self.args.tmpl)
+        self._template = loads(template_path)
+        return self._template
 
-def corpus_loadfile(parsed_args):
-    db = args_to_db(parsed_args)
-    path = parsed_args.path
-    # TODO stream this, don't slurp it.
-    text = slurp(path)
-    return process_text(text, path, db)
+    @property
+    def template_abspath(self):
+        if not self.args:
+            return None
+        return join(TEMPLATES, '{}.{}'.format(self.args.tmplname, DEFAULT_TEMPLATE_EXT))
 
-def poem_new(parsed_args):
-    template = args_to_template(parsed_args)
-    db = args_to_db(parsed_args)
-    poem_lines = poem_from_template(template, db)
-    output_filename = parsed_args.output
-    if output_filename:
-        with open(output_filename, 'w') as f:
-            f.write(map(lambda l: l.get('raw'), poem_lines).join('\n') + '\n')
-            print('poem written to {}'.format(output_filename))
-    else:
-        for line in poem_lines:
-            print(line.get('raw'))
+    def add_dbhost(self):
+        self.add_argument('--host', action='store', default=MONGO_HOST)
+        return self
 
-def abs_template_path(parsed_args):
-    template_name = parsed_args.tmplname
-    return join(TEMPLATES, '{}.{}'.format(template_name, DEFAULT_TEMPLATE_EXT))
+    def add_dbport(self):
+        self.add_argument('-p', '--port', action='store', default=MONGO_PORT)
+        return self
 
-def template_ls(parsed_args):
-    template_files = filter(lambda s: not s.startswith('.'), listdir(TEMPLATES))
-    # TODO this will be weird once we support non json templates:
-    without_ext = map(lambda s: s.replace('.' + DEFAULT_TEMPLATE_EXT, ''), template_files)
-    for filename in without_ext:
-        print(filename)
+    def add_dbname(self):
+        self.add_argument('-d', '--dbname', action='store', default=DEFAULT_DB)
+        return self
 
-def template_rm(parsed_args):
-    template_abspath = abs_template_path(parsed_args)
-    rm(template_abspath)
+    def add_db(self):
+        self.add_dbhost().add_dbport().add_dbname()
+        return self
 
-def template_new(parsed_args):
-    template_abspath = abs_template_path(parsed_args)
-    editor = environ.get('EDITOR', 'vi')
-    cp(EXAMPLE_TEMPLATE, template_abspath)
-    call([editor, template_abspath])
+    def read_template(self, filename):
+        if filename.startswith('/'):
+            path = filename
+        else:
+            path = join(TEMPLATES, '{}.{}'.format(filename, DEFAULT_TEMPLATE_EXT))
 
-def template_edit(parsed_args):
-    template_abspath = abs_template_path(parsed_args)
-    editor = environ.get('EDITOR', 'vi')
-    call([editor, template_abspath])
+        return slurp(path)
+
+    # Chaning helpers:
+
+    def add_argument(self, *args, **kwargs):
+        super().add_argument(*args, **kwargs)
+        return self
+
+    def set_defaults(self, *args, **kwargs):
+        super().set_defaults(*args, **kwargs)
+        return self
+
+    # Actually define commands:
+
+    def corpus_ls(self):
+        for dbname in self.client.database_names():
+            print(dbname)
+
+    def corpus_rm(self):
+        self.client.drop_database(self.args.dbname)
+
+    def corpus_loadfile(self):
+        # TODO stream this, don't slurp it.
+        text = slurp(self.args.path)
+        return process_text(text, self.args.path, self.db)
+
+    def poem_new(self):
+        template = self.template
+        poem_lines = poem_from_template(self.template, self.db)
+        output_filename = self.args.output
+        if output_filename:
+            with open(output_filename, 'w') as f:
+                f.write(map(lambda l: l.get('raw'), poem_lines).join('\n') + '\n')
+                print('poem written to {}'.format(output_filename))
+        else:
+            for line in poem_lines:
+                print(line.get('raw'))
+
+    def template_ls(self):
+        template_files = filter(lambda s: not s.startswith('.'), listdir(TEMPLATES))
+        # TODO this will be weird once we support non json templates:
+        without_ext = map(lambda s: s.replace('.' + DEFAULT_TEMPLATE_EXT, ''), template_files)
+        for filename in without_ext:
+            print(filename)
+
+    def template_rm(self):
+        rm(self.template_abspath)
+
+    def template_new(self):
+        cp(EXAMPLE_TEMPLATE, self.template_abspath)
+        call([self.editor, self.template_abspath])
+
+    def template_edit(self):
+        call([self.editor, self.template_abspath])
+
+    def dispatch(self):
+        self.args = self.parse_args()
+        # TODO try catch
+        try:
+            self.args.func()
+        except AttributeError:
+            self.print_help()
+            return 1
+
+        return 0
+
+def initialize_arg_parser():
+    parser = ProsaicArgParser()
+    subparsers = parser.add_subparsers()
+
+    corpus_parser = subparsers.add_parser('corpus')
+    corpus_subs = corpus_parser.add_subparsers()
+
+    poem_parser = subparsers.add_parser('poem')
+    poem_subs = poem_parser.add_subparsers()
+
+    template_parser = subparsers.add_parser('template')
+    template_subs = template_parser.add_subparsers()
+
+    parser.add_argument('infile', nargs='?', type=FileType('r'), default=sys.stdin)
+
+    # corpus commands
+    corpus_subs.add_parser('ls').set_defaults(func=parser.corpus_ls).add_db()
+    corpus_subs.add_parser('loadfile') \
+               .set_defaults(func=parser.corpus_loadfile) \
+               .add_argument('path', action='store') \
+               .add_db()
+    corpus_subs.add_parser('rm') \
+               .set_defaults(func=parser.corpus_rm) \
+               .add_dbhost() \
+               .add_dbport() \
+               .add_argument('dbname', default=DEFAULT_DB, action='store')
+
+    # poem commands
+    poem_subs.add_parser('new') \
+             .set_defaults(func=parser.poem_new) \
+             .add_argument('-r', '--tmpl_raw', action='store_true') \
+             .add_argument('-o', '--output', action='store') \
+             .add_argument('-t', '--tmpl', action='store', default='haiku') \
+             .add_db()
+
+    # template commands
+    template_subs.add_parser('ls').set_defaults(func=parser.template_ls)
+    template_subs.add_parser('rm') \
+                 .set_defaults(func=parser.template_rm) \
+                 .add_argument('tmplname', action='store')
+    template_subs.add_parser('new') \
+                 .set_defaults(func=parser.template_new) \
+                 .add_argument('tmplname', action='store')
+    template_subs.add_parser('edit') \
+                 .set_defaults(func=parser.template_edit) \
+                 .add_argument('tmplname', action='store')
+
+    return parser
