4f16f4721d306bfb897e11ca46bce0fb4e006033
nathaniel smith <nathanielksmith@gmail.com>
2016-06-01 14:14:42 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,23 @@ from setuptools import setup
 
 setup(
     name='prosaic',
-    version='3.5.4',
+    version='4.0.0',
     description='prose scraper & cut-up poetry generator',
     url='https://github.com/nathanielksmith/prosaic',
     author='vilmibm shaksfrpease',
     author_email='nks@lambdaphil.es',
     license='GPL',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Other Audience',
         'Topic :: Artistic Software',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
     keywords='poetry',
     packages=['prosaic'],
-    install_requires = ['pymongo==2.7.2', 'nltk==3.0.5', 'numpy==1.9.0', 'sh==1.11'],
+    install_requires = ['nltk==3.0.5',
+                        'numpy==1.9.0',
+                        'SQLAlchemy==1.0.12',
+                        'pyhocon==0.3.29',
+                        'psycopg2==2.6.1',],
     include_package_data = True,
     entry_points = {
           'console_scripts': [
