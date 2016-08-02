eb9e4b3e49d7ca9677f4d7847db723b04b777058
nathaniel smith <nathanielksmith@gmail.com>
2016-08-01 21:02:14 -0700

    WIP it more

--- prosaic/parsing.py
+++ prosaic/parsing.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from io import TextIOBase
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 import logging
 import re
 from time import sleep
@@ -79,6 +79,7 @@ def process_sentences(source_id, line_offset, sentences):
 
         session.add(phrase)
 
+    log.debug('finished processing sentences...')
     session.commit()
     session.close()
 
@@ -86,57 +87,7 @@ def process_text(source: Source, text: TextIOBase) -> None:
     """Given raw text and a source filename, adds a new source with the raw
     text as its content and then processes all of the phrases in the text."""
 
-
-    # single loop reading from the buffer. what to do about sentence
-    # boundaries? i'd prefer not to read a character at a time. However, I
-    # think it's okay to lose some stuff at the chunk boundaries for now.
-    # faster performance means more room to do sentence splitting, and i
-    # theorize this will yield net more sentences.
-
-    # if float ids, would need to check the incrementing in the sentence
-    # splitter to do
-
-    # I think the float id idea is too complex for now; i want to be more
-    # incremental. I will use a process pool for the final sentence processing
-    # but have everything be synchronous until then.
-
-    # Going to start with a process pool executor that creates a DB connection
-    # when it's ready to add phrases. this means that i may end up with
-    # partially committed sources.o
-
-    # Option one with this is to just let that happen. Users can delete and
-    # re-add sources if they look incomplete or fucked up. Other option is to
-    # detect when a given chunk future fails, shut down the pool, and then
-    # delete the source.
-
-    # In an absolutely ideal world, i'd be using nested DB transactions. But
-    # going with processes over threads means I can't share that DB handle
-    # between the processes.
-
-    # line_no = 0
-    # futures = []
-    # executor = "TODO"
-    # loop over buffer, reading N kb
-        # pre_process the chunk
-        # extract list of sentences
-        # split_sentences = [] # pairs of id, sentence
-        # for each sentence
-          # attempt to split into subclauses
-          # for each subsentence
-            # add to split_sentences
-        # futures.append(executor.submit(aplit_sentences))
-    # loop over all futures until all done or one exception'd
-    # if one failed, cancel all futures and delete source from db
-
-    # within each submit:
-    # do all of the nlp operations
-    # get a db handle
-    # commit each phrase object
-
-
-    # IDEA is there a way to use two pools? one to do the subtasks in the
-    # lead-up work before parallelizing?
-    CHUNK_SIZE = 2000
+    CHUNK_SIZE = 16000
     line_offset = 0
     futures = []
     ultimate_text = ''
@@ -160,16 +111,12 @@ def process_text(source: Source, text: TextIOBase) -> None:
             line_offset += len(sentences)
             chunk = text.read(CHUNK_SIZE)
 
-        done = []
-        while len(done) != len(futures):
-            log.debug('waiting for chunks to finish processing...')
-            for fut in futures:
-                if fut.done():
-                    if fut.exception():
-                        raise fut.exception
-                    else:
-                        done.append(fut)
-            sleep(1)
+        log.debug('waiting for chunks to finish processing...')
+        for fut in as_completed(futures):
+            if fut.exception() is not None:
+                # TODO if error, cancel all futures and delete source.
+                print('raising exception')
+                raise fut.exception()
 
     log.debug('updating content for source')
     source.content = ultimate_text
