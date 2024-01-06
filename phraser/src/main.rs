be62b4056cd354c0655d897e2ea02e255f9e98f6
Nate Smith <nsmith@archive.org>
2024-01-06 13:13:27 -0800

    continue phraser

--- phraser/src/main.rs
+++ phraser/src/main.rs
@@ -1,10 +1,42 @@
 use std::io;
+use std::env;
+use std::process;
+
+use serde_json::json;
+
+
+fn to_jsonl(source_name: &String, phrase_buff: String) -> String {
+    json!({
+        "source": source_name,
+        "raw": phrase_buff,
+    }).to_string()
+}
 
 fn main() {
-    // TODO source name argument
-    for line in io::stdin().lines() {
-        println!("LINE {}", line.unwrap());
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("need exactly one argument, a source name (got {})", args.len());
+        process::exit(1);
     }
 
-    // TODO
+    let source_name = &args[1];
+    let mut phrase_buff = String::from("");
+
+    for line in io::stdin().lines() {
+        for c in line.unwrap().chars() {
+            if c == ';' || c == ',' || c == ':' || c == '.' || c == '?' || c == '!' {
+                if phrase_buff.len() >= 20 {
+                    println!("{}", to_jsonl(source_name, phrase_buff))
+                }
+                phrase_buff = String::from("");
+                // TODO check phrase_buff len
+                // TODO print jsonl or not
+                // TODO reset phrase buff
+            } else {
+                phrase_buff.push(c);
+            }
+        }
+    }
+
 }
